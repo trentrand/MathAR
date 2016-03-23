ab6ede5eeb06481e8b808b1c78b7fdb0254705b3
Trent Rand <Trentmrand@gmail.com>
2016-03-22 20:47:32 -0700

    Proper Character functionality and basis for LevelManager
    + Implemented Character class with basic functionality for number
    characters
    + Implemented LevelManager class as singleton
    ~ Prefabs with proper PlaceHolder and scale

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -3,28 +3,99 @@ using System.Collections;
 
 public class Character : MonoBehaviour
 {
-
+    /*
+     * AnimState 0 = idle (manually apply root motion for sway effect)
+     * AnimState 1 = walking
+    */
     private Animator animator;
+
+    /*
+    * CharacterType.Hundreds = 100
+    * CharacterType.Tens = 10
+    * CharacterType.One = 1
+    */
+    public LevelManager.CharacterType characterType;
+
+    // Constructor to set character type and initial variables
+    public Character(LevelManager.CharacterType characterType)
+    {
+        this.characterType = characterType;
+    }
 
     void Awake()
     {
+        // Get reference to Animator
         animator = GetComponent<Animator>();
+        StartIdleAnimation();
     }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    if (Input.GetKey(KeyCode.Space))
-	    {
-	        animator.SetInteger("AnimState", 1);
-	    }
-	    else
-	    {
-	        animator.SetInteger("AnimState", 0);
-	    }
-	}
+    void Start()
+    {
+        // TODO: remove after state macine is implemented
+        TraversePath();
+    }
+
+    void Update()
+    {
+
+    }
+
+    // Traverse iTween path
+    void TraversePath()
+    {
+        StartWalkAnimation();
+
+        // (int)characterType returns 100/10/1, depending on enum value
+        iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("Path" + (int)characterType),
+       "orienttopath", true, "time", 7, "easetype", "linear", "onComplete", "FinishedTraversingPath"));
+    }
+
+    // Called when iTween.MoveTo() path traversal is complete
+    void FinishedTraversingPath()
+    {
+        iTween.LookTo(gameObject, iTween.Hash("looktarget", transform.position + new Vector3(0,0,0.1f), "axis", "y"));
+        StartIdleAnimation();
+        DropPlaceValues();
+    }
+
+    // Instantiate 00 or 0 prefabs and drop next to Character gameObject
+    void DropPlaceValues()
+    {
+        switch (characterType)
+        {
+            case LevelManager.CharacterType.Hundreds:
+                GameObject hundredsPlaceHolder = Instantiate(LevelManager.Instance.hundredsPlaceHolderPrefab);
+                hundredsPlaceHolder.transform.parent = transform.FindChild("PlaceHolder").transform;
+
+                hundredsPlaceHolder.transform.rotation = transform.rotation;
+                hundredsPlaceHolder.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+                hundredsPlaceHolder.transform.localScale = Vector3.one;
+
+                break;
+            case LevelManager.CharacterType.Tens:
+                GameObject tensPlaceHolder = Instantiate(LevelManager.Instance.tensPlaceHolderPrefab);
+                tensPlaceHolder.transform.parent = transform.FindChild("PlaceHolder").transform;
+
+                tensPlaceHolder.transform.rotation = transform.rotation;
+                tensPlaceHolder.transform.localPosition = new Vector3(0.0f, 0.0f, 0.0f);
+                tensPlaceHolder.transform.localScale = Vector3.one;
+
+                break;
+        }
+    }
+
+    // Switch Animator active state to Idle and apply root motion (for sway effect)
+    void StartIdleAnimation()
+    {
+        animator.applyRootMotion = true;
+        animator.SetInteger("AnimState", 0);
+    }
+
+    // Switch Animator active state to Walk and disable root motion (since iTween takes care of movement)
+    void StartWalkAnimation()
+    {
+        animator.applyRootMotion = false;
+        animator.SetInteger("AnimState", 1);
+    }
+
 }
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public enum CharacterType
+    {
+        Hundreds = 100,
+        Tens = 10,
+        One = 1
+    };
+
+    public GameObject hundredsPlaceHolderPrefab;
+    public GameObject tensPlaceHolderPrefab;
+
+    // Object declared as a singleton, can be accessed using LevelManager.Instance from anywhere
+    private static LevelManager instance = null;
+    public static LevelManager Instance
+    { 
+        get { return instance; }
+    }
+
+    // Ensures two copies of the singleton do not exist
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+}
