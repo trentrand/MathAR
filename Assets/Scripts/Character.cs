b950f01be2abacfd25c42b0a11d1daa85c5bdaba
Trent Rand <Trentmrand@gmail.com>
2016-03-22 17:25:33 -0700

    Implemented basic animation states

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Character : MonoBehaviour
+{
+
+    private Animator animator;
+
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.Space))
+	    {
+	        animator.SetInteger("AnimState", 1);
+	    }
+	    else
+	    {
+	        animator.SetInteger("AnimState", 0);
+	    }
+	}
+}
